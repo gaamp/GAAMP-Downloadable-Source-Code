3073e7cbefb596bc34d2b23e2ca1bd0c6b484234
GAAMP Code To Parameterize Small Organic Molecules <32204145+gaamp@users.noreply.github.com>
2018-02-06 21:43:18 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd opt/
+tar -zxvf nlopt-2.4.2.tar.gz
+
+cd nlopt-2.4.2
+./configure --enable-static --prefix=$PWD --with-pic
+make && make install
+
+
+cd ../../src 
+make
+
+exit 0
+
